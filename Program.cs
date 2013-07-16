8810887a1ecf6e1fde0752442dc9bb9e393896c5
Nathan McGinn <nmcginn@users.noreply.github.com>
2013-07-16 08:55:05 -0500

    Adds support for command-line arguments

--- Program.cs
+++ Program.cs
@@ -5,32 +5,51 @@ using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Drawing.Imaging;
+using System.IO;
 
 /* PixelTest
  *   Fun little program to play with pixels
  *   Creates an Ulam's Spiral with randomly colored primes
  *     Note: Code is still somewhat sloppy but it's an improvement dammit
  * Written by Nathan McGinn
- *   Last Modified 07/15/2013
+ *   Last Modified 07/16/2013
  */
 
 namespace PixelTest
 {
-    class Program
+   partial class Program
     {
-        // Class variables (to make less argument-intensive method calls)
-        static int width = 801, height = 801;
-        static int pixelY = 400, pixelX = 400; // half of width,height; should be truncated to lower value
-        static int pixelNum = 1, movement = 1;
-        static Random r = new Random();
-
         // Main - Program Entry Point
         static void Main(string[] args)
         {
-            bool toright = true;
-            string path = @"C:\Users\Derek\Desktop\primes.png";
+            // check to see if arguments have been passed in
+            switch (args.Length)
+            {
+                case 0:
+                    break;
+                // case 1 assumes width/height was the variable passed in
+                case 1:
+                    width = Int32.Parse(args[0]);
+                    height = width;
+                    pixelX = width / 2;
+                    pixelY = height / 2;
+                    break;
+                // case 2 assumes width/height and path
+                case 2:
+                    width = Int32.Parse(args[0]);
+                    height = width;
+                    pixelX = width / 2;
+                    pixelY = height / 2;
+                    path = Path.GetFullPath(args[1]);
+                    break;
+                default:
+                    break;
+            }
 
+            // set up the required variables
+            bool toright = true;
             Bitmap bmp = new Bitmap(width, height);
+
             Console.WriteLine("Starting calculations...");
 
             // make sure 1 is filled in
@@ -67,65 +86,6 @@ namespace PixelTest
 
             Console.WriteLine("Image saved, press enter key to close...");
             Console.ReadLine();
-        }
-
-        // MovePixels draws in prime pixels in 1 direction
-        static void MovePixels(Dir direction, ref Bitmap bmp)
-        {
-            for (int i = 1; i <= movement; i++)
-            {
-                // handle direction-specific tasks (movement)
-                switch (direction)
-                {
-                    case Dir.Down:
-                        pixelNum++;
-                        pixelY++;
-                        break;
-                    case Dir.Up:
-                        pixelNum++;
-                        pixelY--;
-                        break;
-                    case Dir.Left:
-                        pixelNum++;
-                        pixelX--;
-                        break;
-                    case Dir.Right:
-                        pixelNum++;
-                        pixelX++;
-                        break;
-                    default:
-                        break;
-                }
-                // handle universal tasks (pixel coloration)
-                if (IsPrime(pixelNum))
-                {
-                    int red = r.Next(0, 225);
-                    int green = r.Next(0, 225);
-                    int blue = r.Next(0, 225);
-                    bmp.SetPixel(pixelX, pixelY, Color.FromArgb(red, green, blue));
-                }
-                else
-                {
-                    bmp.SetPixel(pixelX, pixelY, Color.White);
-                }
-            }
-        }
-
-        // returns true if prime, false if composite
-        static bool IsPrime(int x)
-        {
-            for (int i = 2; i <= Math.Sqrt(x); i++)
-            {
-                if (x % i == 0) return false;
-            }
-            if (x > 1) return true;
-            return false;
-        }
-
-        // Enum to control MovePixels direction
-        enum Dir
-        {
-            Up, Down, Left, Right
         }
     }
 }
---
+++ Utility.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* PixelTest Helper Methods
+ *   Called from the main program
+ * Written by Nathan McGinn
+ *   Last Modified 07/16/2013
+ */
+
+namespace PixelTest
+{
+    partial class Program
+    {
+        // Class variables (to make less argument-intensive method calls)
+        static int width = 801, height = 801;
+        static int pixelY = 400, pixelX = 400; // half of width,height; should be truncated to lower value
+        static int pixelNum = 1, movement = 1;
+        static string path = @"C:\Users\Derek\Desktop\primes.png";
+        static Random r = new Random();
+
+        // MovePixels draws in prime pixels in 1 direction
+        static void MovePixels(Dir direction, ref Bitmap bmp)
+        {
+            for (int i = 1; i <= movement; i++)
+            {
+                // handle direction-specific tasks (movement)
+                switch (direction)
+                {
+                    case Dir.Down:
+                        pixelNum++;
+                        pixelY++;
+                        break;
+                    case Dir.Up:
+                        pixelNum++;
+                        pixelY--;
+                        break;
+                    case Dir.Left:
+                        pixelNum++;
+                        pixelX--;
+                        break;
+                    case Dir.Right:
+                        pixelNum++;
+                        pixelX++;
+                        break;
+                    default:
+                        break;
+                }
+                // handle universal tasks (pixel coloration)
+                if (IsPrime(pixelNum))
+                {
+                    int red = r.Next(0, 225);
+                    int green = r.Next(0, 225);
+                    int blue = r.Next(0, 225);
+                    bmp.SetPixel(pixelX, pixelY, Color.FromArgb(red, green, blue));
+                }
+                else
+                {
+                    bmp.SetPixel(pixelX, pixelY, Color.White);
+                }
+            }
+        }
+
+        // returns true if prime, false if composite
+        static bool IsPrime(int x)
+        {
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0) return false;
+            }
+            if (x > 1) return true;
+            return false;
+        }
+    }
+
+    // Enum to control MovePixels direction
+    enum Dir
+    {
+        Up, Down, Left, Right
+    }
+}
